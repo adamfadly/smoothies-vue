eb15251e1d666e67053f62538ebd90b2f3801757
adam fadly <adamfadly11@gmail.com>
2019-12-09 17:53:41 +0700

    WIP adding ingredients

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <Navbar />
     <router-view/>
+    <AddSmoothie/>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
 
+
 export default {
   name: 'App',
   components: {
     Navbar,
+
   }
 }
 </script>
